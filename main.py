51e0e25c768a8b402f6ebc60f24d941a5cefa381
th119 <mongotianxuhe@gmail.com>
2022-07-27 09:57:21 +0800

    update

--- main.py
+++ main.py
@@ -2,16 +2,22 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import subprocess
+
 from pynput.keyboard import GlobalHotKeys
-import time
+import os
+import sys
+import subprocess
+
+BASE_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
+suspend_path = os.path.join(BASE_DIR, "pssuspend.exe")
+
 def on_activate_h():
     # subprocess.call('start C:\Games\ps_pause\pssuspend.exe MonsterHunterRise.exe')
-    subprocess.call('cmd /c start pssuspend.exe MonsterHunterRise.exe')
+    subprocess.call(f'cmd /c start {suspend_path} MonsterHunterRise.exe')
     print('<ctrl>+<alt>+h pressed')
 
 def on_activate_i():
-    subprocess.call('cmd /c start pssuspend.exe -r MonsterHunterRise.exe')
+    subprocess.call(f'cmd /c start {suspend_path} -r MonsterHunterRise.exe')
     print('<ctrl>+<alt>+i pressed')
 
 # Press the green button in the gutter to run the script.
